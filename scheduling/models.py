8f84f61b4c5a2bc6952f7f6ce0fe142099f1dd38
Jerry Liu <personjerry@gmail.com>
2023-04-19 13:55:30 -0600

    Initialize models

--- scheduling/models.py
+++ scheduling/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# "Add Group" -> dropdown of langauges -> dropdown of groups "Don't worry you can add more later"
+
+class Language(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class LearningGroup(models.Model):
+    name = models.CharField(max_length=200)
+    language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
+
+
+class UserProfile(models.Model):
+    is_teacher = models.BooleanField()
+    learning_groups = models.ManyToManyField(LearningGroup)
+
+
+class Lesson(models.Model):
+    time = models.DateTimeField()
+    description = models.CharField(max_length=200)
+    teacher = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING, null=True, blank=False)
+    learning_group = models.ForeignKey(LearningGroup, on_delete=models.DO_NOTHING)
